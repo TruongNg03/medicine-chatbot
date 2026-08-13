6a663cea409376817c9ada3a674221299207999c
truongng <truongng1312@gmail.com>
2026-08-14 00:26:57 +0700

    create: api create many

--- worker/worker.js
+++ worker/worker.js
@@ -27,6 +27,43 @@ export default {
       );
     }
 
+    // create many
+    const SOURCE_URL =
+      "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnRsIDGZbcEufhjZUHevjJ-_irczOucEm1rM5QZxF8bAS2CUU7tGwTq6Y1YrFv6WNCf-U-4WLveyXsHqNbBl3wAE-xf-DEdCyiB9ayMepdJp3Dr8O3bM7NT1AZRpemdpBMJE6SzFvYF1pt8HMpw_i7qKJZ7d1wkU47Yo9tyv6WTTdIgdUQiLL-bNZmzqFhJR0_or84eBgLh-4lY5oKBC8Zhlt9fPvifdcShfCbvmi9xSRgfyM6MXvNeAHbM3rbw3PS6krYlI6HRhOrag3ta5zYSAmBZHyw&lib=MizkqIlUU7544HPz7hXKU6ZMs7zNMImyn";
+
+    if (request.method === "GET" && url.pathname === `/medicines/import-many`) {
+      try {
+        const count = await importMedicines(env);
+
+        return new Response(
+          JSON.stringify({
+            success: true,
+            inserted: count,
+          }),
+          {
+            headers: {
+              "Content-Type": "application/json",
+              ...corsHeaders(),
+            },
+          },
+        );
+      } catch (error) {
+        return new Response(
+          JSON.stringify({
+            success: false,
+            error: String(error),
+          }),
+          {
+            status: 500,
+            headers: {
+              "Content-Type": "application/json",
+              ...corsHeaders(),
+            },
+          },
+        );
+      }
+    }
+
     let typeAI = DEFAULT_OPTIONS.DEFAULT_TYPE_AI.GEMINI;
 
     try {
@@ -87,6 +124,88 @@ export default {
     }
   },
 };
+
+// use 1 times
+async function importMedicines(env) {
+  const response = await fetch(SOURCE_URL);
+
+  if (!response.ok) {
+    throw new Error(`Source API error: ${response.status}`);
+  }
+
+  const data = await response.json();
+
+  // Tùy cấu trúc API của bạn
+  const items = Array.isArray(data) ? data : data.data;
+
+  if (!Array.isArray(items)) {
+    throw new Error("Source data is not an array");
+  }
+
+  const statements = items.map((item) => {
+    const medicine = {
+      hoat_chat: item["HOẠT CHẤT"]?.value ?? null,
+      biet_duoc: item["TÊN BIỆT DƯỢC"]?.value ?? null,
+      nhom_tac_dung: item["NHÓM DƯỢC LÝ"]?.value ?? null,
+      ham_luong: item["HÀM LƯỢNG"]?.value ?? null,
+      dang_bao_che: item["DẠNG BÀO CHẾ"]?.value ?? null,
+      dong_goi: item["ĐÓNG GÓI"]?.value ?? null,
+      chi_dinh: item["CHỈ ĐỊNH"]?.value ?? null,
+      lieu_dung: item["LIỀU DÙNG"]?.value ?? null,
+      cach_dung: item["CÁCH DÙNG"]?.value ?? null,
+      chong_chi_dinh: item["CHỐNG CHỈ ĐỊNH - THẬN TRỌNG"]?.value ?? null,
+      tuong_tac_thuoc: item["TƯƠNG TÁC THUỐC"]?.value ?? null,
+      tac_dung_khong_mong_muon: item["TÁC DỤNG KHÔNG MONG MUỐN"]?.value ?? null,
+      duoc_dong_hoc_duoc_luc_hoc:
+        item["DƯỢC ĐỘNG HỌC - DƯỢC LỰC HỌC"]?.value ?? null,
+      luu_y: item["LƯU Ý"]?.value ?? null,
+      tai_lieu_tham_khao: item["TÀI LIỆU THAM KHẢO CHUẨN HOÁ"]?.value ?? null,
+    };
+
+    return env.MEDICARE_AI_DB.prepare(
+      `
+      INSERT INTO medicines (
+        hoat_chat,
+        biet_duoc,
+        nhom_tac_dung,
+        ham_luong,
+        dang_bao_che,
+        dong_goi,
+        chi_dinh,
+        lieu_dung,
+        cach_dung,
+        chong_chi_dinh,
+        tuong_tac_thuoc,
+        tac_dung_khong_mong_muon,
+        duoc_dong_hoc_duoc_luc_hoc,
+        luu_y,
+        tai_lieu_tham_khao
+      )
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `,
+    ).bind(
+      medicine.hoat_chat,
+      medicine.biet_duoc,
+      medicine.nhom_tac_dung,
+      medicine.ham_luong,
+      medicine.dang_bao_che,
+      medicine.dong_goi,
+      medicine.chi_dinh,
+      medicine.lieu_dung,
+      medicine.cach_dung,
+      medicine.chong_chi_dinh,
+      medicine.tuong_tac_thuoc,
+      medicine.tac_dung_khong_mong_muon,
+      medicine.duoc_dong_hoc_duoc_luc_hoc,
+      medicine.luu_y,
+      medicine.tai_lieu_tham_khao,
+    );
+  });
+
+  await env.MEDICARE_AI_DB.batch(statements);
+
+  return items.length;
+}
 
 async function askGemini(message, env) {
   const response = await fetch(
